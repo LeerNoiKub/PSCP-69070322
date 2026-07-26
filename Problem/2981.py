6a1eb5f402e4c711da8ed9cfddf423c3d99ada14
LeerNoiKub <69070322@kmitl.ac.th>
2026-07-26 23:07:55 +0700

    W

---
+++ Problem/2981.py
@@ -0,0 +1,7 @@
+"""Kai"""
+name = input()
+surname = input()
+b = name
+c = surname
+print("Hello",name , surname)
+print(b[0:2]+c[0:2])
---
+++ Problem/3023.py
@@ -0,0 +1,12 @@
+"""Cal"""
+n = int(input())
+sum = 0
+count = 0
+if n == 1:
+    print("1")
+while sum != n:
+    if total != 1:
+        total = n+1
+        print(total)
+    else:
+        print(n)
