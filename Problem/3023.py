6e7ae9251e01c947c8a27cba7d934bda32fa3a52
LeerNoiKub <69070322@kmitl.ac.th>
2026-07-26 23:16:15 +0700

    w

--- Problem/3023.py
+++ Problem/3023.py
@@ -1,12 +1,11 @@
 """Cal"""
 n = int(input())
-sum = 0
+num = 0
 count = 0
 if n == 1:
     print("1")
-while sum != n:
-    if total != 1:
-        total = n+1
-        print(total)
-    else:
-        print(n)
+else:
+    while count < n:
+        count += 1
+        num += int(len(str(count))) +1
+    print(num)
